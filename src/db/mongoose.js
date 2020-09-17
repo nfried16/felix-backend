4d23d32e72c555ef0523b64447b46c5b23af6a90
Nathaniel Friedman <nathaniel.friedman@duke.edu>
2020-09-17 01:19:22 -0400

    changed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/felix', {
+mongoose.connect('mongodb+srv://user:felixjiang@cluster0.hh11q.mongodb.net/felix?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true,
 })
--- src/routes/count.js
+++ src/routes/count.js
@@ -4,16 +4,19 @@ const router = new express.Router();
 
 router.get("/", async(req, res) => {
     const ret = await Count.findOne();
+    if(!ret) {
+        return res.send("0");
+    }
     res.send(ret.count.toString());
 })
 
 router.post("/", async (req, res) => {
     // Put userId from auth to put into body
     const count = await Count.findOne();
-
-    if(!count.count) {
-        count.count = 0;
+    if(count == null) {
+        count = new Count({count: 0});
     }
+
     count.count = count.count + 1;
 
     try {
